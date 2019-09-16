a3bb804b98e4d40981aa2ddca72bcb8e12b25b8c
vinnylinck <vinnylinck@gmail.com>
2019-09-16 20:16:27 -0300

    Final version

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     let allQuestions = QuestionBank()
     var pickedAnswer : Bool = false
     var questionNumber : Int = 0
+    var score : Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -22,8 +23,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let firstQuestion = allQuestions.list[0]
-        questionLabel.text = firstQuestion.questionText
+        nextQuestion()
     }
 
 
@@ -44,12 +44,21 @@ class ViewController: UIViewController {
     
     func updateUI() {
       
+        scoreLabel.text = "Score: \(score)"
+        progressLabel.text = "\(questionNumber + 1) / 13"
+        
+        progressBar.frame.size.width = (view.frame.size.width / 13) * CGFloat(questionNumber + 1)
+        
     }
     
 
     func nextQuestion() {
         if questionNumber <= 12 {
+            
             questionLabel.text = allQuestions.list[questionNumber].questionText
+            
+            updateUI()
+            
         } else {
             
             let alert = UIAlertController(title: "Awesme", message: "You've finished all the questions, do you want to start over?", preferredStyle: .alert)
@@ -70,14 +79,21 @@ class ViewController: UIViewController {
         let correctAnswer = allQuestions.list[questionNumber].answer
         
         if correctAnswer == pickedAnswer {
-            print("you got it")
+            
+            ProgressHUD.showSuccess("Correct")
+            
+            score += 1
+
         } else {
-            print("wrong")
+        
+            ProgressHUD.showError("Wrong!")
         }
     }
     
     
     func startOver() {
+        
+        score = 0
         questionNumber = 0
         nextQuestion()
     }
